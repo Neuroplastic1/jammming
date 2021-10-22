a80338a134f30d6f5393bd592eb107cf2807ab5e
Neuroplastic1 <shafqat.umair@gmail.com>
2021-10-22 13:57:19 -0400

    Added all components as static to build up on

---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+function App() {
+  return (
+    <div>
+  <h1>Ja<span class="highlight">mmm</span>ing</h1>
+  <div class="App">
+    < SearchBar />
+    <div class="App-playlist">
+      <SearchResults />
+      < Playlist />
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default App;
